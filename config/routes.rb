5356abfcd9b4f87888a29fe3c25240cd47591940
vagrant <vagrant@localhost.localdomain>
2020-06-01 07:32:49 +0000

    [Fix]N:1の関係の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :users,only: [:show,:index,:edit,:update] do
+  	resources :books,except: [:new]
+  end
   devise_for :users
   root 'home#top'
   get 'home/about'

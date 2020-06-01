d44e96f5b78e16ba096f9a345d4acf418bb157db
vagrant <vagrant@localhost.localdomain>
2020-06-01 07:48:50 +0000

    [Fix]ルーティングdeviseの位置の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users,only: [:show,:index,:edit,:update] do
   	resources :books,except: [:new]
   end
-  devise_for :users
   root 'home#top'
   get 'home/about'
 end

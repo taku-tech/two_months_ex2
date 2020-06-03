c91c7daaa13714d0299a91dbc37ea7d0a5cec1c5
vagrant <vagrant@localhost.localdomain>
2020-06-03 14:47:21 +0000

    [Add]relationshipsのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about' => 'home#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+  	resources :relationships,only: [:index,:create,:destroy]
+  end
   resources :books,except: [:new] do
   	resources :book_comments,only: [:create, :destroy]
   	resource :favorites,only: [:create,:destroy]

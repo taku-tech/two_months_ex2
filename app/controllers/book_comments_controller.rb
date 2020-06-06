05fad7f52925144d25f26e0aeddb347d02afd94d
vagrant <vagrant@localhost.localdomain>
2020-06-06 18:58:11 +0000

    [Update]コメント機能にajaxを実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,17 +5,13 @@ class BookCommentsController < ApplicationController
 		@book = Book.find(params[:book_id])
     	@book_comment = current_user.book_comments.new(book_comment_params)
     	@book_comment.book_id = @book.id
-    	if @book_comment.save
-    		redirect_to book_path(@book)
-    	else
-    		render 'books/show'
-    	end
+    	@book_comment.save
 	end
 
 	def destroy
-		book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-		book_comment.destroy
-    	redirect_to book_path(params[:book_id])
+		@book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+		@book_comment.destroy
+    	@book = Book.find(params[:book_id])
 	end
 
 	private

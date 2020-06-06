0c5bfc6291c3234e7b6cabc79a591718b7c22d0d
vagrant <vagrant@localhost.localdomain>
2020-06-06 16:53:48 +0000

    [Update]いいね機能をajaxで実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,18 +2,14 @@ class FavoritesController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		book = Book.find(params[:book_id])
-        favorite = current_user.favorites.new(book_id: book.id)
-        if favorite.save
-		 	redirect_back(fallback_location: user_path(current_user))
-		end
+		@book = Book.find(params[:book_id])
+        favorite = current_user.favorites.new(book_id: @book.id)
+        favorite.save
 	end
 
 	def destroy
-		book = Book.find(params[:book_id])
-        favorite = current_user.favorites.find_by(book_id: book.id)
-        if favorite.destroy
-        	redirect_back(fallback_location: user_path(current_user))
-        end
+		@book = Book.find(params[:book_id])
+        favorite = current_user.favorites.find_by(book_id: @book.id)
+        favorite.destroy
 	end
 end
